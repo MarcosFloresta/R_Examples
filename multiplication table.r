b502f84e49174d9ac6d55ed84773a08be4a58d94
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:39:04 -0300

    Create multiplication table.r

---
+++ multiplication table.r
@@ -0,0 +1,8 @@
+# R Program to find the multiplicationtable (from 1 to 10)
+# take input from the user
+num = as.integer(readline(prompt = "Enter a number: "))
+# use for loop to iterate 10 times
+for(i in 1:10) {
+print(paste(num,'x', i, '=', num*i))
+}
+print(paste(num,'x', i, '=', num*i))
