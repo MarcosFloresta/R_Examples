4096f50bf5c39489ba36cd55682ee4c8397a13a0
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:55:16 -0300

    Create convert_binary_number_to_decimal.r

---
+++ convert_binary_number_to_decimal.r
@@ -0,0 +1,7 @@
+# Program to convert decimal number into binary number using recursive function
+convert_to_binary <- function(n) {
+if(n > 1) {
+convert_to_binary(as.integer(n/2))
+}
+cat(n %% 2)
+}
