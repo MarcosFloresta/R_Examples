98a584c9d0da8c7270dd2ba584540b4491ca06f0
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:39:07 -0300

    Create hello_world_program.r

---
+++ hello_world_program.r
@@ -0,0 +1,9 @@
+> # We can use the print() function
+> print("Hello World!")
+[1] "Hello World!"
+> # Quotes can be suppressed in the output
+> print("Hello World!", quote = FALSE)
+[1] Hello World!
+> # If there are more than 1 item, we can concatenate using paste()
+> print(paste("How","are","you?"))
+[1] "How are you?"
