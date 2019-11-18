68dbc1ebf35a139b26e33a1e131c0fbf511ca623
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:39:13 -0300

    Create generate_random_number_from_standard_normal_distribution.r

---
+++ generate_random_number_from_standard_normal_distribution.r
@@ -0,0 +1,6 @@
+> rnorm(1)    # generates 1 random number
+[1] 1.072712
+> rnorm(3)    # generates 3 random number
+[1] -1.1383656  0.2016713 -0.4602043
+> rnorm(3, mean=10, sd=2)    # provide our own mean and standard deviation
+[1]  9.856933  9.024286 10.822507
